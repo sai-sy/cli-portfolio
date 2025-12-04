f94f5c0cc418057cdb9c4b88daf2163e0e9a9241
sai <saihaansyed2003@gmail.com>
2025-12-04 16:12:14 -0500

    speed change

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -27,7 +27,7 @@ export default function Home({
         <TypeText
           text="I build this page using Vite, React, Typescript, TSX and am hosting it on Railway. My goal is eventually make this my portfolio site that can be used like a command line while still being visually appealing and usable by a normal user" 
           delay={300}
-          speed={40}
+          speed={20}
           animate={transitionIn}
           fileDir = {null}
           onComplete={() => {
